f75abe1d1e529b472583c56a5b15b51da4de35ab
Miles Bradt <smbradtmichael@gmail.com>
2019-02-09 16:48:42 -0800

    redo popular now

--- src/app/mock-items.ts
+++ src/app/mock-items.ts
@@ -3,5 +3,7 @@ import { Item } from './items/models/item.model'
 
 export const ITEMS: Item [] = [
   new Item("Celeste", "Any%",  "Placeholder"),
-
+  new Item("Super Mario 64", "70 Star",  "Placeholder"),
+  new Item("Ocarina of Time", "No im/ww",  "Placeholder"),
+  new Item("Paper Mario", "Glitchless",  "Placeholder"),
 ]
